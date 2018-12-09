049ef1cb0a0e0309ecd9a97dceed350a530d8074
Luke Tuthill <lukemtuthill@gmail.com>
2018-12-09 16:55:53 +1100

    DRY + default response

--- server.py
+++ server.py
@@ -2,18 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+def send_text(string):
+    return jsonify({"fulfillmentText": string})
+
 @app.route('/assistant', methods=['POST'])
 def assistant():
     json = request.get_json()
     text = json['queryResult']['queryText']
+    print(json)
     if "hello there" in text.lower():
-        return jsonify({
-            "fulfillmentText": "General Kenobi! You are a bold one!"
-            })
-    elif "eidolon" in text.lower():
-        return jsonify({
-            "fulfillmentText": "I'm not 100% sure, sorry."
-            })
+        return sendtext("General Kenobi! You are a bold one!")
+    if "eidolon" in text.lower():
+        return send_text("I'm not 100% sure, sorry.")
+    return send_text("I'm sorry, not sure what you mean by that.")
 
 if __name__ == '__main__':
     app.run()
