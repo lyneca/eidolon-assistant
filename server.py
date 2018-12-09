9e677fca1d5e279d046f91fc4edec6845c7b28c3
Luke Tuthill <lukemtuthill@gmail.com>
2018-12-09 16:43:00 +1100

    Added some responses

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/assistant', methods=['POST'])
 def assistant():
-    print(request.get_json())
-    return ""
+    json = request.get_json()
+    text = json['queryResult']['queryText']
+    if text.lower().contains("hello there"):
+        return jsonify({
+            "fulfillmentText": "General Kenobi! You are a bold one!"
+            })
+    elif text.contains("eidolon"):
+        return jsonify({
+            "fulfillmentText": "I'm not 100% sure, sorry."
+            })
 
 if __name__ == '__main__':
     app.run()
